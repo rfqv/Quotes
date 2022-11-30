c5a5c0773b6fea1cedba415f7af1aaad77b5029c
rfqv <rifqivirgiansyah@gmail.com>
2022-11-30 14:15:47 +0700

    update1

---
+++ app/src/main/java/com/if1b/quotes/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.if1b.quotes.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.if1b.quotes.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
