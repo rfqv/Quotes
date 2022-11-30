60d516ef69a0d446e931a6d9ce01aa1c9d2185db
rfqv <rifqivirgiansyah@gmail.com>
2022-11-30 15:10:42 +0700

    update3

---
+++ app/src/main/java/com/if1b/quotes/API/APIRequestData.java
@@ -0,0 +1,13 @@
+package com.if1b.quotes.API;
+
+import com.if1b.quotes.Model.QuotesModel;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface APIRequestData {
+    @GET("quotes")
+    Call<List<QuotesModel>> requestData();
+}
---
+++ app/src/main/java/com/if1b/quotes/Model/QuotesModel.java
@@ -0,0 +1,9 @@
+package com.if1b.quotes.Model;
+
+public class QuotesModel {
+    private String text, author;
+
+    public String getAuthor() {
+        return author;
+    }
+}
